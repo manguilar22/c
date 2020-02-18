1585e7e5ff9c31e4d9a07fac7682116a48d8b5f9
Manuel Aguilar <manguilar22@gmail.com>
2020-02-18 09:38:40 -0700

    .

--- assignment/tokenizer/main.c
+++ assignment/tokenizer/main.c
@@ -29,12 +29,12 @@ int main()
 	char *p = word_start(&a);
 	printf("Do Something Cooler:\t%c\n",*p);
 
-	 char arr[MAX_SIZE] = "ab bb cc";
+	 char arr[MAX_SIZE] = "  ba bb cc";
 	 char test[MAX_SIZE];
 	 char *pa = word_start(arr);
-	 printf("%c\n",*pa);
-	 printf("%c\n",*word_start(test));
-
+	 char *pe = word_end(arr);
+	 int count = count_words(arr);
+	 printf("\n[+]Word Start:\t%c\n[+]Word End:\t%c\n[+]Word Count:\t%d\n",*pa,*pe,count);
 
      return 0;
 
@@ -43,53 +43,73 @@ int main()
 /* Counts the number of words in the string argument. */
 int count_words(char *str)
 {
+    str = word_start(str);
+    if (str == 0)
+        return 0;
+    int wordCount = 0;
+    while (str != 0)
+    {
+        str = word_start(str);
+        str = word_end(str);
+        wordCount++;
+    }
+    return wordCount;
 
 }
 
+/**
+ * https://stackoverflow.com/questions/1726302/removing-spaces-from-a-string-in-c
+ */
 /* Returns a pointer to the first character of the next
    space-separated word in zero-terminated str.  Return a zero pointer if
    str does not contain any words. */
 char *word_start(char *str)
 {
+    int i;
+
     if ( str == NULL) {
         return 0;
     }
-    for (int i = 0; i < sizeof(str); i++)
+    while (str[i] != '\0')
     {
-        if ( ! (str[i] != ' '))
+        if (non_space_char(str[i]) != 0)
         {
-            return str;
+            return &str[i];
         }
+        i++;
     }
-    return str;
 }
 
 /* Returns a pointer to the first space character in
    zero-terminated str.  Return a zero pointer if
    str does not contain any space characters. */
-char *word_end(char *s)
+char *word_end(char *str)
 {
-
+    int i = 0;
+    while (str[i] != '\0') {
+        if (space_char(str[i]) == 1)
+            return &str[i];
+        i++;
+    }
+    return 0;
 }
 
 int space_char(char c)
 {
-
-	if ((c == ' ' || c == '\t' )&& c != '\0') 
-	{
-		return 1; //true 
-	}
-
-	return 0; //false  
-
+//	if ((c == ' ' || c == '\t' )&& c != '\0')
+if ( c == ' ' || c == '\t')
+{
+    return 1;
+}
+return 0;
 }
 
 int non_space_char(char c) 
 {
-	if ((c != '\t' || c !=' ') && c != '\0')
-	{
-		return 1; 
-	}
-	return 0; 
-	
+//	if ((c != '\t' || c !=' ') && c != '\0')
+if (c == ' ' || c == '\t' || c == '\0') {
+    return 0;
+}
+    return 1;
+
 }
